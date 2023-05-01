ea25f518e5e603d99ab51a3617602d88ca43d681
Andrzej Matusiak <andrzej.matusiak@msg-global.com>
2023-05-01 06:20:08 +0000

    make connection to sap cloud connector

--- srv/lib/handlers.js
+++ srv/lib/handlers.js
@@ -1,78 +1,38 @@
-const { getSoapService } = require('./soap-service');
+const { getSoapService, createClient } = require('./soap-service');
 
 let userReadServicePromise = null;
 let userReadServiceEndpoint = { url: null };
 
+
 (async function () {
+    
     // Connect to external S/4HC SOAP service
-    userReadServicePromise = getSoapService('UserRead', './srv/external/zbind_iscd_scpos.wsdl', userReadServiceEndpoint);
+   userReadServicePromise = getSoapService();
+
 })();
 
 /*** HANDLERS ***/
 
 async function readPayment(req) {
-    try {
-        // Get the SOAP client for the UserRead service
+       
+    userReadServiceEndpoint
 
-        const userReadService = await userReadServicePromise;
-        userReadService.setEndpoint(userReadServiceEndpoint.url);
-
-        // Set the parameters for the zbind_iscd_scpos method of the sevice
-        const param = {
-            I_POST: 'X',
-            I_SIMU: '',
-            T_MESSAGE_AUFB:{
-                item:{}
-            },
-            T_SCPOS:{
-                item: {
-                    AKTYP: "01",
-                    GPART: "31",
-                    VTREF: "20000000000000000001",
-                    PMTFR: "20230406",
-                    PMTTO: "20230406",
-                    CCODE: "V001",
-                    AMOUNT_TOTAL: "-1234,23",
-                    CURR: "EUR",
-                    HVORG: "6030",
-                    TVORG: "0100",
-                    BLART: "20",
-                    BLTYP: "20",
-                    FAEDN: "20230406", 
-                    BUDAT: "20230406",
-                    BLDAT: "20230406",
-                    PSNGL: "X"
-                }
-            }
-              
-           
-            
-            //Payment: {
-             /*   PersonIDInterval: {
-                    IntervalBoundaryTypeCode: 9,
-                    LowerBoundaryPersonID: "0000000000"
-                },
-                BusinessPartnerRoleCodeInterval: {
-                    IntervalBoundaryTypeCode: 9,
-                    LowerBoundaryBusinessPartnerRoleCode: "000000"
-                }*/
-            //},
-           // QueryProcessingConditions: {
-           //     QueryHitsUnlimitedIndicator: true
-           // }
-        };
-
+    // try{
+    //     await userReadServicePromise.ZISCD_SCPOS_MAINTAIN(param).then((result) => {
+    //         posting = result[0]
+    //     })
+    //     return posting
+    // }catch (e) {
+    //     console.log(e.message)
+    //     return e.message
+    // }
         // Invoke zbind_iscd_scpos method asynchronously and wait for the response
-       const resp = await userReadService.ISCD_SCPOS_MAINTAINAsync(param)
+       //const resp = await userReadService.ISCD_SCPOS_MAINTAINAsync(param)
+       //console.log(resp)
         // Prepare the actual service response
         //console.log(resp)
         // const payments = [];
        
-
-        return resp; //resp[0].T_MESSAGE_AUFB.item[0].TEXT;
-    } catch (err) {
-        req.error(err.code, err.message);
-    }
 }
 
 module.exports = {
--- srv/lib/soap-service.js
+++ srv/lib/soap-service.js
@@ -4,33 +4,77 @@ const connectivity = require('@sap-cloud-sdk/connectivity');
 
 // Helper to create a soap service through the BTP destination service
 async function getSoapService(service, wsdl, endpoint) {
-    // Get the service reference
-    var definition = cds.env.requires[service];
-    // Get the destination data from BTP
-    //const dest = await connectivity.getDestination({ destinationName: definition.credentials.destination });
-    // Get service endpoint
-    //const url = (dest.url.substr(dest.url.length-1,1) === '/') ? dest.url.substr(0,dest.url.length-1) : dest.url;
-    //endpoint.url = url + definition.credentials.path;
-    endpoint.url = definition.credentials.path;
 
-    // Create an httpClient which connects over the BTP destination
-    var httpClient = {
-        request: async function (url, data, callback, exheaders, exoptions) {
-            client.executeHttpRequest(dest, {
-                method: 'POST',
-                url: url,
-                data: data,
-                headers: exheaders
-            }, {...exoptions, fetchCsrfToken: false}).then((result) => {
-                callback(null, result, result.data);
-            }).catch((e) => {
-                callback(e);
-            });
+    const param = {
+        I_POST: 'X',
+        I_SIMU: '',
+        T_MESSAGE_AUFB:{
+            item:{}
+        },
+        T_SCPOS:{
+            item: {
+                AKTYP: "01",
+                GPART: "31",
+                VTREF: "20000000000000000001",
+                PMTFR: "20230406",
+                PMTTO: "20230406",
+                CCODE: "V001",
+                AMOUNT_TOTAL: "-1234,23",
+                CURR: "EUR",
+                HVORG: "6030",
+                TVORG: "0100",
+                BLART: "20",
+                BLTYP: "20",
+                FAEDN: "20230406", 
+                BUDAT: "20230406",
+                BLDAT: "20230406",
+                PSNGL: "X"
+            }
         }
     }
+    const dest = await connectivity.getDestination({destinationName: 'ST1CLNT200'})    
+    // http://ST1/sap/bc/srt/wsdl/flv_10002A111AD1/srvc_url/sap/bc/srt/rfc/sap/ziscd_scpos_maintain/200/zservice_iscd_scpos_maintain/zbind_iscd_scpos
+      const requestConfig = {
+          method: 'POST',
+          url: "http://ST1/sap/bc/srt/wsdl/flv_10002A111AD1/srvc_url/sap/bc/srt/rfc/sap/ziscd_scpos_maintain/200/zservice_iscd_scpos_maintain/zbind_iscd_scpos?sap-client=200",
+          headers: {
+            'Content-Type': 'application/soap+xml; charset=utf-8'
+            },
+        proxy: {
+            host: 'localhost',
+            port: 8887,
+            },
+            data: param
+      }
+
+
+    let posting = []
+
+    const executeHttpRequest = await client.executeHttpRequest(dest, requestConfig, {fetchCsrfToken: false})
+
+    const wsdlClient =  await soap.createClientAsync(requestConfig.url ,{HttpClient: executeHttpRequest})
+    // await wsdlClient.setSecurity(new soap.BasicAuthSecurity({
+    //     username: 'matusiaa',
+    //     password: 'Initial@12345!'
+    // }))
+    if(wsdlClient){
+        wsdlClient.ISCD_SCPOS_MAINTAINAsync(param).then((result) => {
+               console.log(result)
+               posting = result
+           }).catch(e => {
+            console.log(e)
+            return e
+           })
+    }
+    else{
+        console.log('No Client')
+        return false
+    }
+
+   
 
     // Instantiate the service using that http client
-    return soap.createClientAsync(wsdl, { httpClient: httpClient });
+   // return soap.createClientAsync(wsdl, { httpClient: httpClient });
 }
 
 module.exports = {
